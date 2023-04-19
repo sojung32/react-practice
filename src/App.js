1f93af8eb6945c93ac9315803aa5e49c665ee025
sojung32 <sojung0320@gmail.com>
2023-04-19 22:19:09 +0900

    update
    - add Routing
    - seperate App

--- src/App.js
+++ src/App.js
@@ -1,53 +1,18 @@
 import React from 'react';
-import { Breadcrumb, Layout } from 'antd';
-// import { Menu } from 'antd';
-import Logo from "./logo.png";
-import "./styles/style.scss";
-import { HomeOutlined } from '@ant-design/icons';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Menu from "./components/common/MenuCpnt";
-import Swiper from "./components/SwiperCpnt";
-import Tab from "./components/TabCpnt";
+import "./styles/style.scss";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <header>
-        <div className="header-wrap">
-          <div className="logo">
-            <img src={Logo} alt="logo"/>
-          </div>
-          {/* <Menu
-            theme="dark"
-            mode="horizontal"
-            // defaultSelectedKeys={['2']}
-            items={new Array(8).fill(null).map((_, index) => {
-              const key = index + 1;
-              return {
-                key,
-                label: `nav ${key}`,
-              };
-            })}
-          /> */}
-          <Menu/>
-        </div>
-      </header>
-      <main>
-        {/* <Breadcrumb separator=">"
-          items={[
-            {title: <HomeOutlined />},
-            {title: 'List', href: ''},
-            {title: 'App', href: ''},
-          ]}>
-        </Breadcrumb> */}
-        <Swiper/>
-        <Tab/>
-        {/* <div className="site-layout-content">
-          Content
-        </div> */}
-      </main>
-      <footer>Ant Design ©2023 Created by Ant UED</footer>
-    </React.Fragment>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
