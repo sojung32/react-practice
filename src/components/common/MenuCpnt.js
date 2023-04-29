2a743e4011c13b436f157c26722c3cc825161aab
sojung32 <sojung0320@gmail.com>
2023-04-29 22:11:32 +0900

    add menu button

--- src/components/common/MenuCpnt.js
+++ src/components/common/MenuCpnt.js
@@ -1,40 +1,9 @@
 import { Button, Drawer, Menu } from 'antd';
 import { useState } from 'react';
-import { MenuOutlined, CloseOutlined, MailOutlined, AppstoreOutlined, SettingOutlined, SearchOutlined, CarryOutOutlined, UserOutlined } from '@ant-design/icons';
+import { MenuOutlined, CloseOutlined, GiftOutlined, SearchOutlined, CalendarOutlined, CarryOutOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 const logo = process.env.PUBLIC_URL + '/celebring.png';
-
-function getItem(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  };
-}
-const items = [
-  getItem('Navigation One', 'sub1', <MailOutlined />, [
-    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
-    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
-  ]),
-  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-    getItem('Option 5', '5'),
-    getItem('Option 6', '6'),
-    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-  ]),
-  {
-    type: 'divider',
-  },
-  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-    getItem('Option 9', '9'),
-    getItem('Option 10', '10'),
-    getItem('Option 11', '11'),
-    getItem('Option 12', '12'),
-  ]),
-  getItem('Group', 'grp', null, [getItem('Option 13', '13'), getItem('Option 14', '14')], 'group'),
-];
 
 const MenuCpnt = () => {
   const [open, setOpen] = useState(false);
@@ -71,6 +40,10 @@ const MenuCpnt = () => {
           <h2 style={{marginLeft: 15, display: 'inline-block', marginBottom: 30}}>로그인</h2>
         </div>
         <div className="menu-list">
+          <div className="button-list">
+            <Button block size="large"><GiftOutlined/> 일정 등록</Button>
+            <Button block size="large"><CalendarOutlined/> 이벤트 등록</Button>
+          </div>
           <Button type="text" block size="large" style={{textAlign: 'left'}}><SearchOutlined/> 검색</Button>
           <Button type="text" block size="large" style={{textAlign: 'left'}}><CarryOutOutlined/> 이벤트</Button>
           <Button type="text" block size="large" style={{textAlign: 'left'}}><UserOutlined/> 마이페이지</Button>
